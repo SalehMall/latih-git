a04d78578135b2f7c01c78ad90a63e7a3a81c4f4
SalehMall <kemalbadawi@gmail.com>
2022-10-31 03:06:04 -1200

    mengubah warna dan menghapus satu  titik

--- polygon/main.cpp
+++ polygon/main.cpp
@@ -11,8 +11,7 @@ void display(void)
 {
 glClear(GL_COLOR_BUFFER_BIT);
 glBegin(GL_POLYGON);
-glColor3f(0.0,0.0,1.0);
-  glVertex2f(-0.5, 0.5);
+glColor4f(1.0f, 1.0f, 1.0f, 0.0f);//white
   glVertex2f(0, 0.75);
   glVertex2f(0.5, 0.5);
   glVertex2f(0.75, 0);
