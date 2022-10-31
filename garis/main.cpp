80ff0bafdfb548ed3aaeb5c027eebdec3d2826e9
SalehMall <kemalbadawi@gmail.com>
2022-10-31 02:30:31 -1200

    masukan waran putih

--- garis/main.cpp
+++ garis/main.cpp
@@ -11,7 +11,7 @@ glClear (GL_COLOR_BUFFER_BIT);
 
 glPushMatrix ();
 glClearColor(1,1,1,0);
-glColor4f(1.0f, 0.0f, 0.0f, 0.0f);//red
+glColor4f(1.0f, 1.0f, 1.0f, 0.0f);//white
 glBegin(GL_LINES);
 glVertex3f (-0.9f,0.0f,0.0f);
 glVertex3f (0.9f,0.0f,0.0f);
